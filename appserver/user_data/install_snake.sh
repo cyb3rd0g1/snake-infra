b506887a7652d6eadc1e88feae7cdd4389b1f45e
149alexniu149 <zhoutingneyc@gmail.com>
2020-06-29 03:16:47 -0400

    add logs

--- appserver/user_data/install_snake.sh
+++ appserver/user_data/install_snake.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
+echo "installing dependencies"
 yum update -y
 yum install git docker -y 
 service docker start
 usermod -a -G docker ec2-user
 
+echo "getting docker-compose"
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 /usr/local/bin/docker-compose version
 
+echo "cloning repos..."
 cd /home/ec2-user
 git clone https://github.com/149alexniu149/snake-client.git
 git clone https://github.com/149alexniu149/snake-server.git
 
+echo "starting services"
 cd snake-client  && /usr/local/bin/docker-compose up -d
 cd ..
 cd snake-server && /usr/local/bin/docker-compose up -d
